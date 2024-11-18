f0f89f5a05b7b4485aee4d89bf542cfad7ae06a7
Ridley Winters <ridley.grenwood.winters@gmail.com>
2024-11-18 11:58:46 -0800

    Automated commit from monorepo

---
+++ assets/js/02f91629.61a44f7b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4432],{6861:e=>{e.exports=JSON.parse('{"tag":{"label":"ux","permalink":"/blog/tags/ux","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/ux","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
